995d90d9fae306f529bb1056d7f600ddb8d3cde7
cybercrypter2810 <megarockystarman@gmail.com>
2019-02-04 12:18:16 -0800

    AA

---
+++ cybercrypter2810git.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  cybercrypter2810;
+git config --global user.name  cybercrypter2810;
+git config --global user.user cybercrypter2810;
+git config --global user.email megarockystarman@gmail.com;
+git config --list;
