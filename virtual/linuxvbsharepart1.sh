146281febab9a4efd8efb5a2852a8ff872352296
cybercrypter2810 <megarockystarman@gmail.com>
2019-10-03 21:29:35 -0700

    AA

---
+++ virtual/linuxvbsharepart1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo ./media/user/Vbox_GAs_6.0.12/VBoxLinuxAdditions.run;
+reboot;
---
+++ virtual/linuxvbsharepart2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo mkdir ~/Desktop/share;
+sudo mount -t vboxsf metalfolder ~/Desktop/share;
